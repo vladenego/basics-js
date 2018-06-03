5573dfc84c975e24fa048736005b4eaf8c4caed6
vladenego <mistervlad3@gmail.com>
2018-06-03 14:52:21 +0300

    reresolve 1 task

--- Tasks/Resolve_task_1/script.js
+++ Tasks/Resolve_task_1/script.js
@@ -1,12 +1,14 @@
 var array = [1,2,3,4,5,6,7,8,9,10]
-for (var i = 0; i < 11; i++) {
-  if (i < 5) {
-    console.log(0);
+
+  for (var i = 0; i < array.length; i++) {
+    
+    if (array[i] < 5) {
+      console.log(0);
   }
-//   else  {
-//     console.log(1);
-//   }
-if (i > 5) {
-  console.log(i);
-}
+      else if (array[i] == 5) {
+        console.log(5);
+  }
+        else {
+          console.log(array[i]);
+    }
 }

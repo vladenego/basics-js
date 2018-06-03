fc5768eed0503a717a8b2509bcb6d35d5ea3abf1
Viacheslav Yefremov <v.o.yefremov@gmail.com>
2018-06-03 14:35:50 +0300

    Minor fixes for first task

--- Tasks/Resolve_task_1/script.js
+++ Tasks/Resolve_task_1/script.js
@@ -1,12 +1,22 @@
-var array = [1,2,3,4,5,6,7,8,9,10]
-for (var i = 0; i < 11; i++) {
+// var array = [1,2,3,4,5,6,7,8,9,10]
+// for (var i = 0; i < 11; i++) {
+//   if (i < 5) {
+//     console.log(0);
+//   }
+// //   else  {
+// //     console.log(1);
+// //   }
+// if (i > 5) {
+//   console.log(i);
+// }
+// }
+
+var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+for (var i = 0; i < array.length; i++) {
   if (i < 5) {
     console.log(0);
+  } else {
+    console.log(1);
   }
-//   else  {
-//     console.log(1);
-//   }
-if (i > 5) {
-  console.log(i);
-}
 }

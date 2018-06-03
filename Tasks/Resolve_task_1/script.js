997186e4b8a6f4a1fd15d685283e78886164b5c3
Viacheslav Yefremov <v.o.yefremov@gmail.com>
2018-06-03 15:14:37 +0300

    Fixes

--- Tasks/Resolve_task_1/script.js
+++ Tasks/Resolve_task_1/script.js
@@ -1,38 +1,10 @@
-<<<<<<< HEAD
-var array = [1,2,3,4,5,6,7,8,9,10]
-
-  for (var i = 0; i < array.length; i++) {
-    
-    if (array[i] < 5) {
-      console.log(0);
-  }
-      else if (array[i] == 5) {
-        console.log(5);
-  }
-        else {
-          console.log(array[i]);
-    }
-=======
-// var array = [1,2,3,4,5,6,7,8,9,10]
-// for (var i = 0; i < 11; i++) {
-//   if (i < 5) {
-//     console.log(0);
-//   }
-// //   else  {
-// //     console.log(1);
-// //   }
-// if (i > 5) {
-//   console.log(i);
-// }
-// }
-
 var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-for (var i = 0; i < array.length; i++) {
-  if (i < 5) {
+for (var i = 0; i < array.length; i++) {  
+  if (array[i] < 5) {
     console.log(0);
   } else {
-    console.log(1);
+    console.log(array[i]);
   }
->>>>>>> fc5768eed0503a717a8b2509bcb6d35d5ea3abf1
 }
+

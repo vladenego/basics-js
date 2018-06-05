32c29bf772db7d345dca45f6535e18ce8ae1089f
vladenego <mistervlad3@gmail.com>
2018-06-05 14:34:59 +0300

    resolve Second task

--- Tasks/Resolve_task_1/script.js
+++ Tasks/Resolve_task_1/script.js
@@ -1,22 +1,31 @@
-<<<<<<< HEAD
 
-var array = [1,2,3,4,5,6,7,8,9,10]
-
-  for (var i = 0; i < array.length; i++) {
-
-    if (array[i] < 5) {
-      console.log(0);
-  }
-      else if (array[i] == 5) {
-        console.log(5);
-  }
-        else {
-          console.log(array[i]);
-    }
-  }
+// var array = [1,2,3,4,5,6,7,8,9,10]
+//
+//   for (var i = 0; i < array.length; i++) {
+//     if (array[i] < 5) {
+//       array[i] = 0
+//     }  else {
+//       console.log(array[i]);
+//     }
+//   }
 
 
-  
+// var array = [2, 5, 10]
+// for (var i = 0; i < array.length; i++) {
+//   if (array[i] < 5) {
+//     array[i] = 0;
+//   }
+// }
+
+var array = [2, 4, 7, 10, -3, 8, 6, 1]
+for (var i = 0; i < array.length; i++) {
+if ( array[i] % 2 == 0) {
+  console.log(array[i]);
+}
+}
+
+
+
 
 
 
@@ -61,15 +70,3 @@ var array = [1,2,3,4,5,6,7,8,9,10]
 //   }
 //
 // }
-=======
-var array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-for (var i = 0; i < array.length; i++) {  
-  if (array[i] < 5) {
-    console.log(0);
-  } else {
-    console.log(array[i]);
-  }
-}
-
->>>>>>> 997186e4b8a6f4a1fd15d685283e78886164b5c3

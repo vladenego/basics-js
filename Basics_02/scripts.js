3b755ab9ca2e01a024493f1e68a417a2bf7830ca
Viacheslav Yefremov <v.o.yefremov@gmail.com>
2018-06-17 00:35:57 +0300

    Add function examples

---
+++ Basics_02/scripts.js
@@ -0,0 +1,88 @@
+// var str_1 = 'hello';
+// var str_2 = 'world';
+
+// function printString(string) {
+//   console.log(string);
+// }
+
+// printString(str_1);
+
+
+// function concat(fuck, secondString) {
+//   var finalString = fuck + secondString;
+//   console.log(finalString);
+// }
+
+// concat(str_1, str_2);
+
+/* -------- */
+
+
+// var atamas = 'Ilya';
+// var yehorov = 'Vlat';
+// var yefemov = 'Slav'
+
+// function findCock(one, two, three) {
+//   console.log(two);
+// }
+  
+// findCock(yehorov, atamas, yefemov);
+  
+/* ----------- */
+
+
+// function sumNumbers(number_1, number_2) {
+//   return number_1 + number_2;
+// }
+
+
+// var sum = sumNumbers(10, 123);
+// console.log(sum);
+
+/* ----------- */
+
+// Haritonova
+
+// function haritonova(tits, boots) {
+  
+//   return 'Beauty: ' + (tits + boots);
+// }
+
+// var sum = haritonova(2, 8);
+// console.log(sum);
+
+/* ------- */
+
+
+// function isAuthorized(login, password) {
+  // if (login === 'tratata228' && password === '123') {
+  //   return true;
+  // } else {
+  //   return false;
+  // }
+// }
+
+// var login = 'tratata228';
+// var password = '123';
+
+
+// var access = isAuthorized(login, password);
+// console.log(access);
+
+
+/* --------- */
+
+var isAuthorized = function(login, password) {
+  if (login === 'tratata228' && password === '123') {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+var login = 'tratata228';
+var password = '123';
+
+
+var access = isAuthorized(login, password);
+console.log(access);
